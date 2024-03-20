36384404a9c0f0f6f0c0ecf9e119b2b4b7504ead
istorres <ismaeltorres612@gmail.com>
2024-03-20 15:04:05 -0300

    feat: lowplan core

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <h1>Em construção</h1>;
+  return (
+    <>
+      <h1>LowPlan</h1>
+      <p>
+        O primeiro passo, o minimo, o plano mais simples para sair do lugar e se
+        organizar.
+      </p>
+      <br />
+      <p>
+        Essa é a ideia aqui. Ir, um passinho de cada vez rumo a organização e
+        produtividade.
+      </p>
+      <br />
+      <p>
+        Descartar o que é superfluo, ostentavel e exagerado. Dar protagonismo às
+        coisas simples e aproveita-las ao máximo.
+      </p>
+    </>
+  );
 }

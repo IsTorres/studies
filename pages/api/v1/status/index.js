0125e70ff1646673ca76d4a8b2f5bf935fa4ec34
istorres <ismaeltorres612@gmail.com>
2025-04-14 23:34:20 -0300

    feat: finished 'api/v1/status' endpoint and adds some tests to it

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
-import database from 'infra/database.js';
+import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.rows);
-  res.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  
+  const serverVersionResult = await database.query(`SHOW server_version;`);
+  const serverVersionValue = serverVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query(`SHOW max_connections;`);
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [dbName],});
+  const openedConnectionsValue = openedConnections.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependences: {
+      database: {
+        server_version: serverVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: openedConnectionsValue,
+      }
+    }
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,14 @@
-test("api/v1/stauts shold be 200", async () => {
+test("api/v1/stauts tests", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
 
   expect(res.status).toBe(200);
+
+  const responseBody = await res.json();
+
+  const responseBodyDate = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(responseBodyDate);
+
+  expect(responseBody.dependences.database.server_version).toEqual("16.0");
+  expect(responseBody.dependences.database.max_connections).toEqual(100);
+  expect(responseBody.dependences.database.opened_connections).toEqual(1);
 });

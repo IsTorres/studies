341c20e301e66ba19cc2e3cded3b93527d99fea7
istorres <ismaeltorres612@gmail.com>
2025-04-20 21:01:33 -0300

    feat: handle ssl connections

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,21 @@
 import { Client } from "pg";
 
- async function query(queryObject) {
+async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
-    ssl: true
+    ssl: process.env.NODE_ENV === "production"
+  });
+
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
   });
 
   try {
@@ -23,5 +31,5 @@ import { Client } from "pg";
 }
 
 export default {
-  query
-}
+  query,
+};

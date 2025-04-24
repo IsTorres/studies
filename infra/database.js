a8cfa43730568d8f36193f900c3d8a42eceefd1a
istorres <ismaeltorres612@gmail.com>
2025-04-24 00:35:52 -0300

    feat: minor changes

--- infra/database.js
+++ infra/database.js
@@ -10,14 +10,6 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  console.log({
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
-  });
-
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -41,5 +33,5 @@ function getSSLValues() {
     };
   }
 
-  return process.env.NODE_ENV === "production";
+  return process.env.NODE_ENV === "development" ? false : true;
 }

8c5c13c3677d41c385576dd0ce402e6e8991747c
istorres <ismaeltorres612@gmail.com>
2025-04-29 23:01:40 -0300

    chore: remove comentarios e confirma a opção de usar pool de conexões no lugar do client

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,4 @@
-import { Client, Pool } from "pg";
+import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -8,15 +8,6 @@ const pool = new Pool({
 });
 
 async function query(queryObject) {
-  // const client = new Client({
-  //   host: process.env.POSTGRES_HOST,
-  //   user: process.env.POSTGRES_USER,
-  //   database: process.env.POSTGRES_DB,
-  //   password: process.env.POSTGRES_PASSWORD,
-  //   port: process.env.POSTGRES_PORT,
-  //   ssl: getSSLValues(),
-  // });
-
   try {
     await pool.connect();
     const result = await pool.query(queryObject);
@@ -24,22 +15,9 @@ async function query(queryObject) {
   } catch (error) {
     console.error("Erro ao executar query:", error);
     throw error;
-  } 
-  // finally {
-  //   await client.end();
-  // }
+  }
 }
 
 export default {
   query,
 };
-
-// function getSSLValues() {
-//   if (process.env.POSTGRES_CA) {
-//     return {
-//       ca: process.env.POSTGRES_CA,
-//     };
-//   }
-
-//   return process.env.NODE_ENV === "development" ? false : true;
-// }
